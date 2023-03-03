5a4a7e723a54b973394c21d82cdb78bcfc037cd7
RXTArtemis <r_nava20@outlook.com>
2023-03-03 11:40:32 -0600

    new changes

--- index.js
+++ index.js
@@ -1,26 +1,87 @@
 //create variables
-let board = []; //array to push conditions into
-let playerO = "O"; //player O
-let playerX = "X"; //player X
+let board=[[0,1,2]];
+          [[0,1,2]];
+          [[0,1,2]];  //array to push conditions into
+let playerO = img2; //player O
+let playerX = img; //player X
+
+//for individual icon markers
+let img=document.createElement("img");
+img src="https://mir-s3-cdn-cf.behance.net/projects/404/ba0765162472679.63d6958e81afc.jpg";
+document.body.appendChild(img);
+
+let img2=document.createElement("img");
+img2 src="https://64.media.tumblr.com/a135905d66c9fa3a969da082dbdb812f/fb42c1baa3d2d89e-5c/s400x600/57210f3bc2dc8f7a627dd27f4dd667e6ae94fd28.png ";
+document.body.appendChild("img");
+
+
 let currentPlayer = playerX; //start with player x
 //boolean value that will switch once the conditions are complete
 let winner = true;
 let gameOver = false; 
+let turns = 0;
+let cellPlayerMarker = [" ", " ", " ", " "," "," "," ", " ", " "];
+let winConditions=[[0,1,2],
+                   [0,0,0],
+                   [1,1,1],
+                   [2,2,2],
+                   [2,1,0]];
 
-//invoke all functions to create game logic
+
+//individual cells
+let case1=document.getElementById("case1");
+let case2=document.getElementById("case2");
+let case3=document.getElementById("case3");
+let case4=document.getElementById("case4");
+let case5=document.getElementById("case5");
+let case6=document.getElementById("case6");
+let case7=document.getElementById("case7");
+let case8=document.getElementById("case8");
+let case9=document.getElementById("case9");
+
 //press start button
+document.getElementById('start').addEventListener('click',()=>{
+//ask about which function to put here to start the game
+});
+
+
 
 //code that invokes all game functions
 
 
+//switch turns in between players
+//possibly add turn counter stuff here and make turns a global variable
 
+let cells = document.getElementsByClassName("cell");
+console.log(cells);
 
+for(let i=0; i < cells.length; i++){
+    console.log(cells[i]);
+    cells[i].addEventListener("click", ()=>{
+        console.log(cells[i]);
+        cells[i].innerHTML = playerX;
+    });
+}
 
+function turnsCounter(){
+  while(cells[i].innerHTML === playerX && gameOver === false){
+    if(turns % 2 === 0 ){
+        cells[i].innerHTML = playerX;
+        turns++;
+    }else if(turns % 2 === 1){
+        cells[i].innerHTML = playerO;
+        turns++;
+    }
+  }
 
+}
 
-//switch turns in between players-create a function
+const restartButton = document.getElementById('restartButton');
+const winningMessageElement = document.getElementById('winningMessage');
+const winningMessageText = document.getElementById('winningMessageText');
 
-function switchPlayerTurns(playerX, playerO){
+function switchPlayerTurns(){
+
 //set a boolean for players that works only inside this function
 playerX = true;
 //loop to iterate through turns, less than 9 turns because there are 9 boxes but index starts at 0
@@ -29,78 +90,91 @@ for(let i = 0; i < 9; i++){
         //these conditions are both true
         if(currentPlayer === playerX && gameOver === false){
             //function to mark box and play a turn
-            boxMarker(playerX);
-            //update the board-function to update the board
-            updateBoard();
-            chooseWinner(){
+            boxMarker();
+            chooseWinner();
                 if(playerX ===true && winner === true){
             } console.log("Winner is player X!");
-    
-        //ask if updateBoard should be inside loop or outside loop   
+            winningMessageText.innerText = "Winner is Player X!";
+       
         //first condition is false to switch players and second condition is true for playerO to play their turn
-        }else if(currentPlayer!=playerX && gameOver === false){
+        }if(currentPlayer!=playerX && gameOver === false){
             //player O's turn
             //switch players if the first if condition is not met
             currentPlayer = playerO;
-            boxMarker(playerO);
-            updateBoard();
-            //is this correct being here or do I add to the choose winner function or see if you should add to game play 
-            chooseWinner(){
-                if(playerO ===true && winner === true){
+            boxMarker();
+            chooseWinner();
+                if(playerX ===false && winner === true){
             } console.log("Winner is player O!");
+            winningMessageText.innerText = "Winner is Player O!";
     
-                    }
-                } //check if there is a winner every round/turn
-            } 
-
+         }else{
+                winningMessageText.innerText = "Its a draw!"
          }
+                } 
+            }
+
+        }
+
+
+
+//function to shade box when there is a winner
+function boxMarker(){
+ 
+    for(let i = 0; i < 9; i++){
+        while(winner === true && gameOver === true){
+            cells[i].style.backgroundColor = "light-grey";
+        }
+    }if(turns % 2 === 0){
+        cells[i].style.background = "url(images/isabellewaving.jpg)";
+    
+    }else if(turns % 2 === 1){
+        cells[i].style.background ="url(images/tomnook.jpg)" ;
+        
     }
 }
 
-//function to mark box and individual cell
-function boxMarker(){
-//add marker to every clickable cell and bring icons to JS
 
-}
 
 //function to choose winner and announce winner(add conditions in this function)
 function chooseWinner(){
-   
-    //create conditions for a win
-    let winConditions=[[1,2,3],
-                       [1,4,7],
-                       [1,5,9],
-                       [4,5,6],
-                       [2,5,8],
-                       [3,5,7],
-                       [7,8,9],
-                       [3,6,9]];
-    for(let i = 0; i < 9; i++){
-    while(!winner && gameOver){
-    //check for conditions  and update board
-         }if(!winConditions){
-            //game continues if win conditions aren't met
+    for(let i = 0; i < winConditions.length; i++){
+        let condition = winConditions[i];
+        const box1 = condition[0];
+        const box2 = condition[1];
+        const box3 = condition[2];
+        if(box1 === " " || box2 === " " || box3 === " "){
             gameOver===false;
-         }else{
-            winner === true;
+        }else if(box1 == box2 && box2 ==box3){
+            winner===true;
             gameOver===true;
-            //made the board not clickable except for the reset button
-            //declare winner and make it appear on the page
-            
-         }
+        }
     }
 }
 
-//function to update Board data
-function updateBoard(board){
- //set board indexes to resemble a grid
- board=[[0,0,0]],
-       [[0,0,0]];
-       [[0,0,0]];
-
-}
 
 
 //activate button to start and reset game
+function resetGame(){
+   restartButton;
+   gameOver=true;
+   document.getElementById(case1).value = " ";
+   document.getElementById(case2).value = " ";
+   document.getElementById(case3).value = " ";
+   document.getElementById(case4).value = " ";
+   document.getElementById(case5).value = " ";
+   document.getElementById(case6).value = " ";
+   document.getElementById(case7).value = " ";
+   document.getElementById(case8).value = " ";
+   document.getElementById(case9).value = " ";
+    for(i=0; i < cells.length; i++){
+        console.log(cells[i]);
+        cells[i].removeEventListener("click", ()=>{
+            console.log(cells[i]);
+            cells[i].innerHTML = currentPlayer;
+        }); 
+}
+
+}
+
 
 
